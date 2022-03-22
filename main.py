3330a40ede39f1cce9fd40cf138fb2cb24d313cb
rafikli <rafael.libertini@gmail.com>
2022-03-21 22:57:43 -0300

    INSERT & DELETE item carrinho

--- main.py
+++ main.py
@@ -91,3 +91,43 @@ def read_carrinho(id_carrinho : uuid.UUID):
         return base["carrinho"][id_carrinho]
     except:
         raise HTTPException(status_code=404)
+
+@app.post("/carrinho/{id_carrinho}/{id_item}", status_code = 201)
+def update_item_carrinho(id_carrinho: uuid.UUID, id_item: uuid.UUID, item: Item):
+    try:
+        if id_carrinho not in base["carrinho"]:
+            raise HTTPException(status_code=404, detail="Carrinho not found")
+
+        if id_item not in base["inventario"]:
+            raise HTTPException(status_code=404, detail="Item not found")
+
+        if id_item not in base["carrinho"][id_carrinho].items.keys():
+            base["carrinho"][id_carrinho].items.update({ id_item: item })
+
+        else:   
+            base["carrinho"][id_carrinho].items[id_item].quantity += item.quantity
+        return base["carrinho"]
+    except:
+        raise HTTPException(status_code=404)
+
+@app.delete("/carrinho/{id_carrinho}/{id_item}")
+def delete_item_carrinho(id_carrinho: uuid.UUID, id_item: uuid.UUID, item: Item):
+    try:
+        if id_carrinho not in base["carrinho"]:
+            raise HTTPException(status_code=404)
+
+        if id_item not in base["inventario"]:
+            raise HTTPException(status_code=404)
+
+        if id_item not in base["carrinho"][id_carrinho].items.keys():
+            raise HTTPException(status_code=404)
+
+        else:   
+            base["carrinho"][id_carrinho].items[id_item].quantity -= item.quantity
+
+            if  base["carrinho"][id_carrinho].items[id_item].quantity <= 0:
+                base["carrinho"][id_carrinho].items.pop(id_item, False)
+            
+        return base["carrinho"]
+    except:
+        raise HTTPException(status_code=404)
